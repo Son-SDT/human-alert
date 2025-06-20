55dea903b90a898e0008890f3e1a252304da867b
Khim3 <nhatkhiem003@gmail.com>
2025-06-20 19:56:15 +0700

    change alert sound, convert from cv dnn to yolo.hub, add sound for alerting

--- app.py
+++ app.py
@@ -1,15 +1,15 @@
 import cv2
 import numpy as np
 from imutils.video import VideoStream
+from test import *
 
-
+# use webcam as input video stream
 video = VideoStream(src=0).start()
-# Chua cac diem nguoi dung chon de tao da giac
+# Points of polygons
 points = []
 
 # new model Yolo
-#model = YoloDetect()
-
+model = YoloDetect()
 
 def handle_left_click(event, x, y, flags, points):
     if event == cv2.EVENT_LBUTTONDOWN:
@@ -19,7 +19,6 @@ def handle_left_click(event, x, y, flags, points):
 def draw_polygon (frame, points):
     for point in points:
         frame = cv2.circle( frame, (point[0], point[1]), 5, (0,0,255), -1)
-
     frame = cv2.polylines(frame, [np.int32(points)], False, (255,0, 0), thickness=2)
     return frame
 
@@ -29,11 +28,11 @@ while True:
     frame = video.read()
     frame = cv2.flip(frame, 1)
 
-    # Ve ploygon
+    # plot the polygon
     frame = draw_polygon(frame, points)
 
-   # if detect:
-     #   frame = model.detect(frame= frame, points= points)
+    if detect:
+       frame = model.detect(frame= frame, points= points)
 
     key = cv2.waitKey(1)
     if key == ord('q'):
